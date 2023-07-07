d3194861b9155b420e711469cc8673272de77561
Horror26 <1032210438@tcetmumbai.in>
2023-07-07 15:39:43 +0530

    cors problem solved

--- server/index.js
+++ server/index.js
@@ -21,12 +21,7 @@ database.connect();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-	cors({
-		origin:"http://localhost:3000",
-		credentials:true,
-	})
-)
+app.use(cors());	
 
 app.use(
 	fileUpload({
