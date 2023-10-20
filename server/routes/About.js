7da05934719ebd4564948f55b833703c36b6752b
Lyubomir Ternavskiy <127299159+LyubomirT@users.noreply.github.com>
2023-10-20 09:03:13 +0300

    Update About.js

--- server/routes/About.js
+++ server/routes/About.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const router = express.Router();
 
+// Import the About page component from "./src/pages/About.jsx"
+const AboutPage = require("../src/pages/About.jsx");
+
+// Define a route for the "/about" URL path
+router.get("/about", (req, res) => {
+  // Render the About page
+  res.render("about", { pageContent: AboutPage });
+});
+
+module.exports = router;
