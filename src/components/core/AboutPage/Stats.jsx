74def8d31eb0c95f23e5b27304c189a960c58ca2
Horror26 <1032210438@tcetmumbai.in>
2023-10-28 21:41:06 +0530

    counter animation added

--- src/components/core/AboutPage/Stats.jsx
+++ src/components/core/AboutPage/Stats.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import CountUp from "react-countup";
 
 const Stats = [
-  { count: "5K", label: "Active Students" },
-  { count: "10+", label: "Mentors" },
-  { count: "200+", label: "Courses" },
-  { count: "50+", label: "Awards" },
+  { count: "500", label: "Active Students" },
+  { count: "10", label: "Mentors" },
+  { count: "100", label: "Courses" },
+  { count: "50", label: "Ratings" },
 ];
 
-const StatsComponenet = () => {
+const StatsComponent = () => {
   return (
     <div className="bg-richblack-700">
       {/* Stats */}
-      <div className="flex flex-col gap-10 justify-between w-11/12 max-w-maxContent text-white mx-auto ">
+      <div className="flex flex-col gap-10 justify-between w-11/12 max-w-maxContent text-white mx-auto">
         <div className="grid grid-cols-2 md:grid-cols-4 text-center">
           {Stats.map((data, index) => {
             return (
               <div className="flex flex-col py-10" key={index}>
                 <h1 className="text-[30px] font-bold text-richblack-5">
-                  {data.count}
+                  <CountUp duration={10} end={data.count} />+
                 </h1>
                 <h2 className="font-semibold text-[16px] text-richblack-500">
                   {data.label}
@@ -31,4 +32,4 @@ const StatsComponenet = () => {
   );
 };
 
-export default StatsComponenet;
+export default StatsComponent;
