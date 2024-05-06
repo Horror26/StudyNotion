9069a38f90ae0549a9bb67c0ba5b2f037998061e
aryanpachchigar <aryanpachchigar12@gmail.com>
2024-05-06 11:54:00 +0530

    updated

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -77,7 +77,7 @@ function Navbar() {
                           : "text-richblack-25"
                       }`}
                     >
-                      <p>{link.title}</p>
+                       <p>{link.title}</p>
                       <BsChevronDown />
                       <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
                         <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
