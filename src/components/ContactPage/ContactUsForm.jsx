79fb7d3749d8e922636683574745ab6ad6c97919
PaulDev <13600558+dev-paulL@users.noreply.github.com>
2024-10-20 14:15:26 +0200

    style: add loading animation on contact form submit button (resolves #51)

--- src/components/ContactPage/ContactUsForm.jsx
+++ src/components/ContactPage/ContactUsForm.jsx
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 import useWeb3Forms from "@web3forms/react";
 import { ToastContainer } from "react-toastify";
 import { toast } from "react-hot-toast";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 const accessKey = process.env.REACT_APP_ACCESS_KEY;
 
@@ -11,31 +11,40 @@ export default function ContactUsForm() {
   const { register, handleSubmit, reset } = useForm();
   const [isSuccess, setIsSuccess] = useState(false);
   const [result, setResult] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const { submit: onSubmit } = useWeb3Forms({
     access_key: accessKey,
     onSuccess: (msg, data) => {
       setIsSuccess(true);
       setResult(msg);
+      setIsLoading(false);
       reset();
       toast.success("Form submitted successfully!");
     },
     onError: (msg, data) => {
       setIsSuccess(false);
       setResult(msg);
+      setIsLoading(false);
       toast.error(`Error: ${msg}`);
     },
   });
 
+  const handleSubmitContactForm = (data) => {
+    setIsLoading(true);
+    onSubmit(data);
+  };
+
   return (
     <div>
-      <form className="flex flex-col gap-7" onSubmit={handleSubmit(onSubmit)}>
+      <form className="flex flex-col gap-7" onSubmit={handleSubmit(handleSubmitContactForm)}>
         <div className="flex flex-col gap-5 lg:flex-row">
           <div className="flex flex-col gap-2 lg:w-[48%]">
             <label htmlFor="name" className="label-style">
               Name
             </label>
             <input
+              disabled={isLoading}
               type="text"
               name="name"
               id="name"
@@ -49,6 +58,7 @@ export default function ContactUsForm() {
               Email Address
             </label>
             <input
+              disabled={isLoading}
               type="email"
               name="email"
               id="email"
@@ -64,6 +74,7 @@ export default function ContactUsForm() {
             Message
           </label>
           <textarea
+            disabled={isLoading}
             name="message"
             id="message"
             cols="30"
@@ -76,10 +87,17 @@ export default function ContactUsForm() {
 
         <button
           type="submit"
-          className="rounded-md bg-yellow-50 px-6 py-3 text-center text-[13px] font-bold text-black shadow-[2px_2px_0px_0px_rgba(255,255,255,0.18)] 
+          className="flex items-center justify-center rounded-md bg-yellow-50 px-6 py-3 text-center text-[13px] font-bold text-black shadow-[2px_2px_0px_0px_rgba(255,255,255,0.18)] 
            sm:text-[16px]"
+          disabled={isLoading}
         >
-          Submit Form
+          {isLoading ? (
+            <>
+              <div className="spinner h-4 w-4 border-black" /> <span className="ml-2">Submitting...</span>
+            </>
+          ) : (
+            "Submit Form"
+          )}
         </button>
       </form>
 
