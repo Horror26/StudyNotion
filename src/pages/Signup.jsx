cfa4c22b0a9c5f96d301ed3abde4a24a40860dc2
Shlok Prajapati <shlokprajapati2003@gmail.com>
2024-10-27 23:59:00 +0530

    added text for signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,7 +6,7 @@ function Signup() {
     <Template
       title="Unlock Your Coding Potential with StudyNotion"
       description1="Learn in-demand skills for the future and start building your dream career today."
-      description2="Join millions of learners and kickstart your coding journey today."
+      description2="Join thousands of learners and kickstart your coding journey today."
       image={signupImg}
       formType="signup"
     />
